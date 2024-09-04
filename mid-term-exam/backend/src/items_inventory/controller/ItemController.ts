f969311cdf0fb9f444bd4be917c0b68b6478fbbb
David Cárdenas <santito500@live.com>
2024-09-04 17:24:10 -0500

    feat: :sparkles: Created the Item controller
    This controller will return all items stored in the db. In addition it will add items to the database receiving an array of its corresponding JSON objects.

--- mid-term-exam/backend/src/items_inventory/controller/ItemController.ts
+++ mid-term-exam/backend/src/items_inventory/controller/ItemController.ts
@@ -1,17 +1,22 @@
+import APIItem from "../model/interfaces/APIItem";
 import ItemService from "../services/ItemService";
 import { Request, Response } from "express";
 
 export default class ItemController {
   constructor(private readonly itemService: ItemService) {}
 
-  public async getAllItems(req: Request, res: Response): Promise<any> {
+  public async getAllItems(_req: Request, res: Response): Promise<any> {
     const items = await this.itemService.getAllItems();
     res.status(200).json({ items: items });
   }
 
-  public async createItems(req: Request, res: Response): Promise<any> {
-    const { name, vendor, rating, image_path } = req.body;
-    await this.itemService.addItems(name);
-    res.status(201).json({ message: "Item created" });
+  public async addItems(req: Request, res: Response): Promise<any> {
+    const apiItems = req.body as APIItem[];
+    const result = await this.itemService.addItems(apiItems);
+    if (!result) {
+      res.status(500).json({ message: "Error creating item/s" });
+      return;
+    }
+    res.status(201).json({ message: "Item/s created on the database" });
   }
 }
