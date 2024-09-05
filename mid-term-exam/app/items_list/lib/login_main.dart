8b8983d4b154443d0a2048d82db8d5ff230695b2
David Cárdenas <santito500@live.com>
2024-09-04 23:08:42 -0500

    feat: :tada: Begun application design
    Reused some past code for the login view but up for change.

---
+++ mid-term-exam/app/items_list/lib/login_main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:login/login_app.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  try {
+    await dotenv.load(fileName: "assets/.env"); // Load .env file
+  } catch (e) {
+    print('.env file not found');
+  }
+
+  runApp(MaterialApp(
+    home: LoginApp(),
+  ));
+}
