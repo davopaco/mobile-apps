e1e4b21fc87a399e3bafea5bbbc2d28bf5fa6189
David Cárdenas <santito500@live.com>
2024-09-06 18:23:56 -0500

    fix: :zap: State render fix
    Now the widget tree is not fully reloaded. The menu overlay is the only widget state reloading

--- mid-term-exam/app/items_list/lib/components/items/items_view.dart
+++ mid-term-exam/app/items_list/lib/components/items/items_view.dart
@@ -24,13 +24,13 @@ class ItemsView extends StatefulWidget {
 }
 
 class _ItemsViewState extends State<ItemsView> {
-  bool _isMenuOpen = false;
+  final _menuNotifier = ValueNotifier<bool>(false);
   bool _isGridChanged = false;
   late OverlayEntry _overlayEntry;
 
   @override
   void dispose() {
-    if (_isMenuOpen) {
+    if (_menuNotifier.value) {
       _removeOverlay();
     }
     widget.scrollController.dispose();
@@ -50,14 +50,12 @@ class _ItemsViewState extends State<ItemsView> {
   }
 
   void _toggleMenu() {
-    setState(() {
-      _isMenuOpen = !_isMenuOpen;
-      if (_isMenuOpen) {
-        _showOverlay();
-      } else {
-        _removeOverlay();
-      }
-    });
+    _menuNotifier.value = !_menuNotifier.value;
+    if (_menuNotifier.value) {
+      _showOverlay();
+    } else {
+      _removeOverlay();
+    }
   }
 
   OverlayEntry _createOverlayEntry() {
@@ -69,7 +67,7 @@ class _ItemsViewState extends State<ItemsView> {
         child: Material(
           child: SlidingMenu(
             loginUsecase: widget.loginUsecase,
-            isOpen: _isMenuOpen,
+            isOpen: _menuNotifier.value,
             onToggle: _toggleMenu,
           ),
         ),
@@ -106,27 +104,33 @@ class _ItemsViewState extends State<ItemsView> {
                         height: 130, // Adds extra space at the top
                         color: Colors.transparent),
                     Expanded(
-                        child: ItemsList(
-                            itemsUseCase: widget.itemsUseCase,
-                            isGridChanged: _isGridChanged,
-                            key: const PageStorageKey('items_list'),
-                            items: snapshot.data!,
-                            scrollController: widget.scrollController)),
+                        child: ValueListenableBuilder(
+                            valueListenable: _menuNotifier,
+                            builder: (_, context, __) {
+                              return ItemsList(
+                                  itemsUseCase: widget.itemsUseCase,
+                                  isGridChanged: _isGridChanged,
+                                  key: const PageStorageKey('items_list'),
+                                  items: snapshot.data!,
+                                  scrollController: widget.scrollController);
+                            })),
                   ],
                 );
               }
             },
           ),
           Positioned(
-            top: _isMenuOpen ? 245 : 50,
-            right: _isMenuOpen ? MediaQuery.of(context).size.width / 2.35 : 40,
+            top: _menuNotifier.value ? 245 : 50,
+            right: _menuNotifier.value
+                ? MediaQuery.of(context).size.width / 2.35
+                : 40,
             child: ItemCircularButton(
               callback: _toggleMenu,
-              icon: Icon(_isMenuOpen ? Icons.close : Icons.menu),
+              icon: Icon(_menuNotifier.value ? Icons.close : Icons.menu),
             ),
           ),
           Positioned(
-              top: _isMenuOpen ? -100 : 50,
+              top: _menuNotifier.value ? -100 : 50,
               left: 40,
               child: ItemCircularButton(
                 icon: const Icon(Icons.compare_arrows_rounded),
