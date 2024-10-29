8d825f5a3bb4d78c8e6f1dcb2850490022a40c4b
David Cárdenas <santito500@live.com>
2024-10-29 14:47:49 -0500

    refactor: Implemented a new menu
    This menu follows the structure of the SlidingMenu, but using a different approach for animation

--- final-term-exam/app/instamess/lib/components/sliding_menu.dart
+++ final-term-exam/app/instamess/lib/components/sliding_menu.dart
@@ -3,106 +3,79 @@ import 'package:get/get.dart';
 import 'package:instamess/usecases/login_usecase.dart';
 
 class SlidingMenu extends StatefulWidget {
-  final bool isOpen;
-  final VoidCallback onToggle;
   final LoginUseCase loginUsecase;
 
-  const SlidingMenu(
-      {super.key,
-      required this.isOpen,
-      required this.onToggle,
-      required this.loginUsecase});
+  const SlidingMenu({super.key, required this.loginUsecase});
 
   @override
   _SlidingMenuState createState() => _SlidingMenuState();
 }
 
-class _SlidingMenuState extends State<SlidingMenu>
-    with SingleTickerProviderStateMixin {
-  late final AnimationController _controller;
-  late final Animation<Offset> _slideAnimation;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = AnimationController(
-      vsync: this,
-      duration: const Duration(milliseconds: 300),
-    );
-
-    _slideAnimation = Tween<Offset>(
-      begin: const Offset(0, -1),
-      end: const Offset(0, 0),
-    ).animate(CurvedAnimation(
-      parent: _controller,
-      curve: Curves.easeInOut,
-    ));
-
-    _updateAnimation();
-  }
-
-  @override
-  void didUpdateWidget(SlidingMenu oldWidget) {
-    super.didUpdateWidget(oldWidget);
-    if (widget.isOpen != oldWidget.isOpen) {
-      _updateAnimation();
-    }
-  }
-
-  void _updateAnimation() {
-    if (widget.isOpen) {
-      _controller.forward();
-    } else {
-      _controller.reverse();
-    }
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
+class _SlidingMenuState extends State<SlidingMenu> {
   @override
   Widget build(BuildContext context) {
-    return SlideTransition(
-      position: _slideAnimation,
+    return SingleChildScrollView(
       child: Container(
+        padding: const EdgeInsets.only(top: 20),
         decoration: const BoxDecoration(
+          color: Colors.white,
           borderRadius: BorderRadius.only(
-            bottomLeft: Radius.circular(20),
-            bottomRight: Radius.circular(20),
+            topLeft: Radius.circular(30),
+            topRight: Radius.circular(30),
           ),
-          color: Colors.black,
         ),
-        height: 230,
-        width: double.infinity,
         child: Column(
           children: [
-            const SizedBox(height: 50),
+            Container(
+              width: 40,
+              height: 5,
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(255, 122, 122, 122),
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+            const SizedBox(height: 10),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Menu',
+                  style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black),
+                ),
+                SizedBox(width: 10),
+                Icon(
+                  size: 30,
+                  Icons.menu,
+                  color: Colors.black,
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
             ListTile(
-                leading: const Icon(Icons.person, color: Colors.white),
+                leading: const Icon(Icons.person,
+                    color: Color.fromARGB(255, 64, 64, 64)),
                 title: const Text('Usuarios',
-                    style: TextStyle(color: Colors.white)),
+                    style: TextStyle(color: Color.fromARGB(255, 64, 64, 64))),
                 onTap: () {
-                  widget.onToggle();
                   Get.toNamed("/users");
                 }),
             ListTile(
-              leading: const Icon(Icons.message, color: Colors.white),
+              leading: const Icon(Icons.message,
+                  color: Color.fromARGB(255, 64, 64, 64)),
               title: const Text('Mensajes recibidos',
-                  style: TextStyle(color: Colors.white)),
+                  style: TextStyle(color: Color.fromARGB(255, 64, 64, 64))),
               onTap: () {
-                widget.onToggle();
                 Get.toNamed("/messages");
               },
             ),
             ListTile(
                 leading: const Icon(Icons.logout, color: Colors.red),
-                title:
-                    const Text('Logout', style: TextStyle(color: Colors.white)),
+                title: const Text('Logout',
+                    style: TextStyle(color: Color.fromARGB(255, 64, 64, 64))),
                 onTap: () async {
-                  widget.onToggle();
                   await widget.loginUsecase.logout();
                 }),
           ],
--- final-term-exam/app/instamess/lib/components/users/users_view.dart
+++ final-term-exam/app/instamess/lib/components/users/users_view.dart
@@ -23,57 +23,6 @@ class UsersView extends StatefulWidget {
 }
 
 class _UsersViewState extends State<UsersView> {
-  final _menuNotifier = ValueNotifier<bool>(false);
-  late OverlayEntry? _overlayEntry;
-
-  @override
-  void initState() {
-    super.initState();
-    _overlayEntry = _createOverlayEntry();
-  }
-
-  @override
-  void dispose() {
-    if (_menuNotifier.value) {
-      _removeOverlay();
-    }
-    super.dispose();
-  }
-
-  void _toggleMenu() {
-    _menuNotifier.value = !_menuNotifier.value;
-    if (_menuNotifier.value) {
-      _showOverlay();
-    } else {
-      _removeOverlay();
-    }
-  }
-
-  OverlayEntry _createOverlayEntry() {
-    return OverlayEntry(
-      builder: (context) => Positioned(
-        top: 0,
-        left: 0,
-        right: 0,
-        child: Material(
-          child: SlidingMenu(
-            loginUsecase: widget.loginUsecase,
-            isOpen: _menuNotifier.value,
-            onToggle: _toggleMenu,
-          ),
-        ),
-      ),
-    );
-  }
-
-  void _showOverlay() {
-    Overlay.of(context).insert(_overlayEntry!);
-  }
-
-  void _removeOverlay() {
-    _overlayEntry!.remove();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -84,9 +33,43 @@ class _UsersViewState extends State<UsersView> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 const SizedBox(height: 60),
-                const Text(
-                  'Usuarios',
-                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      flex: 1,
+                      child: Container(),
+                    ),
+                    const Expanded(
+                      flex: 2,
+                      child: Center(
+                        child: Text(
+                          textAlign: TextAlign.center,
+                          'Usuarios',
+                          style: TextStyle(
+                              fontSize: 30, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      flex: 1,
+                      child: Align(
+                        alignment: Alignment.center,
+                        child: ItemCircularButton(
+                          callback: () {
+                            showModalBottomSheet(
+                                context: context,
+                                builder: (context) {
+                                  return SlidingMenu(
+                                    loginUsecase: widget.loginUsecase,
+                                  );
+                                });
+                          },
+                          icon: Icon(Icons.menu),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
                 const SizedBox(height: 10),
                 Expanded(
@@ -114,21 +97,6 @@ class _UsersViewState extends State<UsersView> {
                   ),
                 )
               ],
-            ),
-            ValueListenableBuilder(
-              valueListenable: _menuNotifier,
-              builder: (_, context, __) {
-                return Positioned(
-                  top: _menuNotifier.value ? 245 : 50,
-                  right: _menuNotifier.value
-                      ? Get.mediaQuery.size.width / 2.35
-                      : 40,
-                  child: ItemCircularButton(
-                    callback: _toggleMenu,
-                    icon: Icon(_menuNotifier.value ? Icons.close : Icons.menu),
-                  ),
-                );
-              },
             ),
           ],
         ),
